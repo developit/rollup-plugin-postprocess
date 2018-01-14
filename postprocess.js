436aa4e070a8645622c4c2e86d74109766bc1529
Jason Miller <jason@developit.ca>
2018-01-14 11:15:04 -0500

    Initial commit!

---
+++ postprocess.js
@@ -0,0 +1,41 @@
+import MagicString from 'magic-string';
+
+let currentToken;
+const replacer = (str, index) => currentToken[index];
+
+export default function postprocess(allReplacements) {
+	return {
+		name: 'postprocess',
+		transformBundle(code, { sourceMap }) {
+			let str = new MagicString(code);
+			let replacements = typeof allReplacements==='function' ? allReplacements({ code, sourceMap }) : allReplacements;
+
+			for (let i=0; i<replacements.length; i++) {
+				let [find, replace=''] = replacements[i];
+				if (typeof find==='string') find = new RegExp(find);
+				if (!find.global) {
+					find = new RegExp(find.source, 'g' + String(find).split('/').pop());
+				}
+
+				let token;
+				while (token=find.exec(code)) {
+					let value;
+					if (typeof replace==='function') {
+						value = replace.apply(null, token);
+						if (value==null) value = '';
+					}
+					else {
+						currentToken = token;
+						value = replace.replace(/\$(\d+)/, replacer);
+					}
+					str.overwrite(token.index, token.index + token[0].length, value);
+				}
+			}
+
+			return sourceMap===false ? str.toString() : {
+				code: str.toString(),
+				map: str.generateMap({ hires: true })
+			};
+		}
+	};
+}
---
+++ test.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+var assert = require('assert');
+var postprocess = require('.');
+
+var code = 'function t(t,o,e,i){for(i=0,o=o.split?o.split("."):o;t&&i<o.length;)t=t[o[i++]];return void 0===t?e:t}module.exports=t;';
+
+var name, exportPrefix;
+var out = postprocess([
+	[/(module\.exports\s*=\s*|export\s*default\s*)([a-zA-Z$_][a-zA-Z0-9$_]*)[;,]?/, function(str, prefix, id) {
+		name = id;
+		exportPrefix = prefix;
+	}],
+	[/^function\s([a-zA-Z$_][a-zA-Z0-9$_]*)/, function(str, id) {
+		if (id===name) {
+			return exportPrefix + str;
+		}
+		return str;
+	}]
+]).transformBundle(code, { sourceMap: false });
+
+assert.equal(out, 'module.exports=function t(t,o,e,i){for(i=0,o=o.split?o.split("."):o;t&&i<o.length;)t=t[o[i++]];return void 0===t?e:t}');
+console.log('✅ Tests Passed');
+process.exit(0);
